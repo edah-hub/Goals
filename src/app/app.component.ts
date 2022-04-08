bfad74842072cfdcbfd1e6a5e54bfca1f104c127
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-08 17:49:16 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GoalComponent } from './goal/goal.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goal';
+ 
 }
